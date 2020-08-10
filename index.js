c7193d301f9cbe4183cdd135148628fb8db99a8a
SoulHarsh007 <harshtheking@hotmail.com>
2020-08-10 23:57:45 +0530

    Update dependencies. Update code to use UA-Strings

--- index.js
+++ index.js
@@ -1,44 +1,40 @@
-const config = require("./config");
-const fetch = require("node-fetch");
-const url = "https://api.github.com/user";
-const headers = {
-    Authorization: "token " + config.token,
-    "Content-Type": "application/json"
+const config = require('./config');
+const centra = require('@aero/centra');
+const main = async () => {
+  const bioData = await getBio();
+  const status = await postBio(bioData);
+  if (status.login) console.log('Bio updated!');
+  else if (status.message) console.log(`Err: ${status.message}`);
 };
-const url2 =
-    "https://api.openweathermap.org/data/2.5/weather?appid=" +
-    config.weather +
-    "&q=" +
-    config.city;
-async function main() {
-    const bio_data = await getBio();
-    const status = await postBio(bio_data);
-    if (status.login) console.log("Bio updated!");
-    else if (status.message) console.log(`Err: ${status.message}`);
+const getBio = async () => {
+  const response = await centra(
+    'https://api.openweathermap.org/data/2.5/weather',
+    'GET'
+  )
+    .query({appid: config.weather, q: config.city})
+    .json();
+  const today = new Date();
+  const time = today.toLocaleTimeString('it-IT');
+  return (
+    'Current weather in ' +
+    config.city +
+    ': ' +
+    response.weather[0].main.toUpperCase() +
+    ' | Last update: ' +
+    time +
+    ' | Made by SoulHarsh007 with JavaScript!'
+  );
 };
-async function getBio() {
-    const response = await fetch(url2);
-    const json = await response.json();
-    const today = new Date();
-    const time = today.toLocaleTimeString("it-IT");
-    const result =
-        "Current weather in " +
-        config.city +
-        ": " +
-        json.weather[0].main.toUpperCase() +
-        " | Last update: " +
-        time +
-        " | Made by SoulHarsh007 with JavaScript!";
-    return result;
-};
-async function postBio(Bio) {
-    const response = await fetch(url, {
-        method: 'PATCH',
-        headers: headers,
-        body: `{"bio": "${Bio}"}`
-    });
-    const json = await response.json();
-    return json;
+const postBio = async Bio => {
+  const response = await centra('https://api.github.com/user', 'PATCH')
+    .body({bio: Bio}, 'JSON')
+    .header({
+      Authorization: `token ${config.token}`,
+      'User-Agent':
+        'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1',
+    })
+    .json();
+  return response;
 };
 main();
 setInterval(main, 5 * 60 * 1000);
